f360f86b12731122abaafebea60bac267fc6e31a
Eduardo377 <eduardogomes377@gmail.com>
2025-04-11 05:02:03 -0300

    :bug: fix: resolvido

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-    theme: {
-        extend: {},
-    },
     darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                primary: 'var(--primary)',
+                'primary-dark': 'var(--primary-dark)',
+            },
+        },
+    },
     plugins: [],
-};
+}
